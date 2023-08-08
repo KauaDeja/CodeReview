182f85aee359bdad4197565134cd3b6c3237ad38
Kaua Deja <kauakarate@gmail.com>
2023-08-08 13:00:17 -0300

    Vulnerabilidade encontrada e corrigida

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+function get_all_files_dir($user) {
+    $base = basename(realpath("files/".$user));
+    if (!file_exists($base)) {
+        mkdir($base);
+    }
+
+    return array_diff(scandir($base), array("..", '.'));
+    }
+
+// Substitua pelo nome de usuário desejado
+$username = "../../../etc/passwd";
+$files = list_files_for_user($username);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>List Files for User</title>
+</head>
+<body>
+    <h1>Files for User: <?php echo $username; ?></h1>
+    <ul>
+        <?php foreach ($files as $file) : ?>
+            <li><?php echo $file; ?></li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
